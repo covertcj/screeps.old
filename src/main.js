21cab48ca867a5d4ffca26b94eced9d998962070
Christopher Joseph Covert <covertops5@gmail.com>
2016-07-07 13:24:36 -0400

    Basic harvester role.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-var harvesterRole = require('roleHarvester');
+var harvesterRole = require('role.harvester');
 
 var main = Object.freeze({
   loop: function () {
-    _.each(harvesterRole.get(Game.creeps), (h) => harvesterRole.loop(h));
+    var harvesters = harvesterRole.get(Game.creeps);
+
+    if (harvesters.length < 2) {
+      var newHarvester = Game.spawns.Spawn1.createCreep([MOVE, CARRY, WORK], undefined, { role: 'harvester' });
+      console.log('Creating a new harvester: ' + newHarvester);
+    }
+
+    _.each(harvester, harvesterRole.loop);
   }
 });
 
---
+++ src/role.harvester.js
@@ -0,0 +1,29 @@
+var harvester = Object.freeze({
+  loop: function (creep) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if (!sources) return;
+
+    if (creep.carry.energy < creep.carryCapacity) {
+      if (creep.harvest(sources[0]) !== ERR_NOT_IN_RANGE) return;
+
+      creep.moveTo(sources[0]);
+    } else if (Game.spawns.Spawn1.energy < Game.spawns.Spawn1.energyCapacity) {
+      if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) !== ERR_NOT_IN_RANGE) return;
+
+      creep.moveTo(Game.spawns.Spawn1);
+    }
+  },
+
+  create: function (spawn) {
+    console.log('Creating a new harvester: (' + spawn.name + ',' + newHarvester + ')');
+    var newHarvester = spawn.createCreep([MOVE, CARRY, WORK], undefined, { role: 'harvester' });
+  }
+
+  get: function (creeps) {
+    return _.filter(Game.creeps, (c) => c.memory.role === this.type);
+  },
+
+  type: 'harvester'
+});
+
+module.exports = harvester;
